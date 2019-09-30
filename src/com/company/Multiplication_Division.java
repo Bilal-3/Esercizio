1b16cd85d5a3ae91538e17f590fec450bfb97b79
MrMosna <francesco.mosna2000@gmail.com>
2019-09-30 12:14:54 +0200

    Aggiunto moltiplicazione e divisione

---
+++ src/com/company/Multiplication_Division.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Multiplication_Division {
+    private int n1, n2;
+
+    public Multiplication_Division() {
+    }
+
+    public int calcoloMoltiplicazione(int n1, int n2){
+        int rtn = 0;
+        return rtn = n1 * n2;
+    }
+
+    public int calcoloDivisione(int n1, int n2){
+        int rtn = 0;
+        return rtn = n1 / n2;
+    }
+}
